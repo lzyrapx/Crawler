95f21e5b6d1d1bc7f5edefeb307021cd92a507fc
Walker_lzy <1303377506@qq.com>
2017-05-01 22:44:57 +0800

    爬Codeforces,Bestcoder
    爬Codeforces,Bestcoder

---
+++ 爬取cf，bc上的分数.py
@@ -0,0 +1,37 @@
+#coding:utf8
+import sys
+import urllib2,re
+
+#爬取codeforces的分数
+def GetCfRating(name):
+    url = 'http://codeforces.com/profile/' + name
+    html = urllib2.urlopen(url).read()
+    content = html.decode('utf-8')
+    pattern = re.compile('Contest rating.*?">(.*?)</span>',re.S)
+    items = re.findall(pattern,content)
+    for item in items:
+        print item
+
+#爬取bestcoder的分数
+def GetBcRating(name):
+    url = 'http://bestcoder.hdu.edu.cn/rating.php?user=' + name
+    html = urllib2.urlopen(url).read()
+    content = html
+    pattern = re.compile('RATING.*?">(.*?)</span>',re.S)
+    items = re.findall(pattern,content)
+    for item in items:
+        print item
+        
+        
+print "请输入你想在Codeforces上查找的ID分数:"
+s=raw_input()
+GetCfRating(s)
+
+GetCfRating("tourist")
+print "跪烂Orz"
+GetCfRating("quailty")
+print "再次跪烂Orz"
+
+GetCfRating("biGinNer")
+GetCfRating("Raphael_walker")
+GetBcRating("doubility")
