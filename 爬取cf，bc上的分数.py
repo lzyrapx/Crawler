955763b635ce4f4f24e7b66f18f7b6b8294cac63
LzyRapx <1303377506@qq.com>
2018-05-02 22:54:32 +0800

    Update 爬取cf，bc上的分数.py

--- 爬取cf，bc上的分数.py
+++ 爬取cf，bc上的分数.py
@@ -1,36 +1,52 @@
-#coding:utf8
+# coding:utf8
 import sys
-import urllib2,re
+import urllib2, re
 
-#爬取codeforces的分数
+
+# 爬取codeforces的分数
 def GetCfRating(name):
     url = 'http://codeforces.com/profile/' + name
     html = urllib2.urlopen(url).read()
     content = html.decode('utf-8')
-    pattern = re.compile('Contest rating.*?">(.*?)</span>',re.S)
-    items = re.findall(pattern,content)
+    pattern = re.compile('Contest rating.*?">(.*?)</span>', re.S)
+    items = re.findall(pattern, content)
     for item in items:
         print item
 
-#爬取bestcoder的分数
+
+# 爬取bestcoder的分数
 def GetBcRating(name):
     url = 'http://bestcoder.hdu.edu.cn/rating.php?user=' + name
     html = urllib2.urlopen(url).read()
     content = html
-    pattern = re.compile('RATING.*?">(.*?)</span>',re.S)
-    items = re.findall(pattern,content)
+    pattern = re.compile('RATING.*?">(.*?)</span>', re.S)
+    items = re.findall(pattern, content)
     for item in items:
         print item
-        
-        
+
+
 print "请输入你想在Codeforces上查找的ID分数:"
-s=raw_input()
+s = raw_input()
 GetCfRating(s)
 
+GetCfRating("palayutm")
+print "跪烂Orz"
 GetCfRating("tourist")
 print "跪烂Orz"
 GetCfRating("quailty")
 print "再次跪烂Orz"
 
 GetCfRating("biGinNer")
-GetBcRating("doubility")
+
+GetCfRating("doubility")
+
+"""
+2343
+跪烂Orz
+3235
+跪烂Orz
+2491
+再次跪烂Orz
+2159
+"""
+
